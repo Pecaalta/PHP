6d119f8fb2080b5c888b393ff5d04ac9e1d124b5
Lucas Montelongo <37620501+lucasmontelongo@users.noreply.github.com>
2019-06-17 11:36:24 -0300

    un par de bobadas

--- application/controllers/servicio.php
+++ application/controllers/servicio.php
@@ -43,53 +43,6 @@ class Servicio extends CI_Controller {
 		return true;
 	}
 
-    public function nuevoServicio(){
-        $data = array(
-			"nombre" => $this->input->post('nombre'),
-            "is_active" => true,
-            "descripcion" => $this->input->post('descripcion'),
-            "precio" => $this->input->post('precio'),
-            "id_restaurante" => 2,
-            "imagen" => null
-		);    
-		
-		$config = array(
-			array(
-					'field' => $data["nombre"],
-					'label' => 'Nombre de servicio',
-					'rules' => 'required | alpha_numeric_spaces',
-					'errors' => array(
-						'required' => 'El %s es inválido.'
-						)
-			),
-			array(
-					'field' => $data["descripcion"],
-					'label' => 'Descripcion del servicio',
-					'rules' => 'required | alpha_numeric_spaces',
-					'errors' => array(
-							'required' => 'La %s es inválida.'
-					),
-			),
-			array(
-					'field' => $data["precio"],
-					'label' => 'Precio del servicio',
-					'rules' => 'required | decimal | less_than[0]'
-			)
-		);
-	
-		$this->form_validation->set_rules($config);
-
-		if ($this->form_validation->run() === FALSE) {
-			$user = json_decode(json_encode($this->session->userdata('user')), true);
-			$this->load->view('main/navbar', $this->nav);
-			$this->load->view('/restaurante/servicios'.$user["id"]);
-		} else {
-			$data["id"] = $this->model_servicio->insertar($data);
-
-        	redirect("/restaurante/servicios");
-		}
-	}
-
 	public function info_servicio($id)
 	{
 		$servicio = $this->model_servicio->infoServicio($id);
