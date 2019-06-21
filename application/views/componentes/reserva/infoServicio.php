5b7823cdc0b1e02ad97783eae7b621dbcb8ba999
Lucas Montelongo <37620501+lucasmontelongo@users.noreply.github.com>
2019-06-20 22:31:55 -0300

    sd

--- application/views/componentes/reserva/infoServicio.php
+++ application/views/componentes/reserva/infoServicio.php
@@ -2,7 +2,7 @@
     <?php if($item->nombre == $nombreServicio):?>
         <p>Nombre: <span id="nombreComida"><?php echo $item->nombre ?></span></p>
         <p>Precio: <span id="precioComida"><?php echo $item->precio ?></span></p>
-        <p>Mas info: <a href="<?php echo base_url() ?>Servicio/info_servicio/<?php echo $item->id ?>">Ver <?php echo $item->nombre ?></a></p>
+        <p>Mas info: <a target="_blank" href="<?php echo base_url() ?>Servicio/info_servicio/<?php echo $item->id ?>">Ver <?php echo $item->nombre ?></a></p>
         <input type="text" name="idComida" id="idComida" value="<?php echo $item->id ?>" readonly style="display: none">
         <p>Cantidad: <input type="number" id="cantidad" style="width: 30%"></p>
         <a id="enviaComida" href="#">Añadir</a>
